5fe7ef9bfce913c2ab42a594a8fa9e504d78fa08
Allison MacQueen <allimacq@gmail.com>
2020-09-24 18:49:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,55 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end 
+
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end 
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+  #returns false if there is no value 
+  def self.find_by_name(song_name)
+   @@all.find {|songs| songs.name == song_name}
+  end
+  
+   
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name) != nil
+      self.find_by_name(song_name)
+    elsif self.find_by_name(song_name) == nil
+      self.create_by_name(song_name)
+    end
+  end
+   
+   def self.alphabetical
+     all = @@all
+     all.sort_by(&:name)
+   end
+   
+   def self.new_from_filename(song_and_name)
+     #need to use regex to separate song and name string and then create a song.name and song.artist
+     separate = song_and_name.split(/\W/)
+     p separate
+     song = self.new
+     
+   end
+   
+   
+   
+
+    
+
+
 end
