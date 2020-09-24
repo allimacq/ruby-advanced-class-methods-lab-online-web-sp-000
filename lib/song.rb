57b5a1e0f92856d1f66944aa88ba020c1aea5af2
Allison MacQueen <allimacq@gmail.com>
2020-09-24 19:07:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,6 +49,8 @@ class Song
    
    def self.new_from_filename(song_and_name)
      #need to use regex to separate song and name string and then create a song.name and song.artist
+     separate = song_and_name.split(" - ")
+     puts separate
      song_artist = song_and_name.match(/\A\w+/).to_s
      song_name = song_and_name.match(//).to_s
      puts song_artist
